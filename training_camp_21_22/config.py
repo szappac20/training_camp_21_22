52767eccef0502335fbd52209f663f798ee6380e
ZAPPACOSTA STEFANO (DTO) <ZAPPAC20@posteitaliane.it>
2022-07-06 02:22:48 +0200

    Add config and init files

---
+++ training_camp_21_22/config.py
@@ -0,0 +1,6 @@
+import os
+
+base_folder = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(base_folder, "..", "data")
+plot_path = os.path.join(base_folder, "..", "plots")
+submission_path = os.path.join(base_folder, "..", "submissions")
