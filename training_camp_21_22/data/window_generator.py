764a0d8b8a17ad3ffe3ac9bf91912aedc4f7625b
ZAPPACOSTA STEFANO (DTO) <ZAPPAC20@posteitaliane.it>
2022-07-07 22:24:43 +0200

    Add documentation to WindowGenerator class

--- training_camp_21_22/data/window_generator.py
+++ training_camp_21_22/data/window_generator.py
@@ -8,6 +8,42 @@ class WindowGenerator(object):
     def __init__(
             self, input_width, label_width, shift,
             train_df, val_df, test_df, label_columns=None):
+        """
+        Class to manage the windowing of a time-series dataset
+        in terms of sub-sequences of a given length input_width,
+        coupled with label_width predictions.
+
+        The predictions are shifted with shift steps
+        with respect to the inputs.
+
+        train_df, val_df, test_df must be Pandas dataframes
+        with the same columns
+        If no evaluation dataset is available the test dataset can be used
+
+        A single-step window with a single prediction for the next time-step
+        value may be defined as follows
+
+        >>> w2 = WindowGenerator(
+                input_width=1, label_width=1, shift=1,
+                train_df=train_df, val_df, test_df,
+                label_columns=['f1', 'f2', 'f3'])
+        Total window size: 2
+        Input indices: [0]
+        Label indices: [1]
+        Label column name(s): ['f1', 'f2', 'f3']
+
+        Args:
+            input_width (int): number of time-steps used for the prediction
+            label_width (int): number of time-steps to be predicted
+            shift (int): shift between the last time step used for input
+                         and the first used for prediction
+            train_df (Pandas.DataFrame): train dataset
+            val_df (Pandas.DataFrame): evaluation dataset
+            test_df (Pandas.DataFrame): test dataset
+            label_columns (list): list of string denoting the columns
+                                  of the ground truth
+                                  (energy consumptions for f1, f2, f3)
+        """
 
         # Store the raw data
         self.train_df = train_df
@@ -40,6 +76,10 @@ class WindowGenerator(object):
         self._example = self.example
 
     def __repr__(self):
+        """
+        A description of the main aspects of the window
+        through the standard output
+        """
         return "\n".join([
             f"Total window size: {self.total_window_size}",
             f"Input indices: {self.input_indices}",
@@ -47,6 +87,17 @@ class WindowGenerator(object):
             f"Label column name(s): {self.label_columns}"])
 
     def split_window(self, features):
+        """
+        Given a list of consecutive inputs,
+        this method will convert
+        them to a window of inputs and a window of labels.
+
+        The example w2 you define earlier w
+
+        Args:
+            features (np.array): numpy 3-dimensional array
+                                 [num_batches, total_window_size, num_features]
+        """
         inputs = features[:, self.input_slice, :]
         labels = features[:, self.labels_slice, :]
         if self.label_columns is not None:
@@ -62,7 +113,16 @@ class WindowGenerator(object):
         return inputs, labels
 
     def plot(self, model=None, plot_cols=None, max_subplots=3):
+        """
+        Plot the split window showing the inputs and the labels.
+        If a model is passed, the predictions are plotted as well.
 
+        Args:
+            model (keras.Model): keras model object
+            plot_cols (list): columns to be shown
+            max_subplots (int): maximum number of subplots
+
+        """
         c_list = ["blue", "orange", "green"]
         plt.rc("axes", prop_cycle=(cycler.cycler("color", c_list)))
 
@@ -107,6 +167,15 @@ class WindowGenerator(object):
         fig.subplots_adjust(right=0.75)
 
     def make_dataset(self, data, sequence_stride=12, batch_size=32):
+        """
+
+        Args:
+            data (np.array):
+            sequence_stride (int):
+            batch_size (int): size of the batch
+
+        Returns:
+        """
         data = np.array(data, dtype=np.float32)
         ds = tf.keras.utils.timeseries_dataset_from_array(
             data=data,
@@ -140,7 +209,7 @@ class WindowGenerator(object):
         """
         Get and cache an example batch of `inputs, labels` for plotting.
         """
-        result = getattr(self, '_example', None)
+        result = getattr(self, "_example", None)
         if result is None:
             # No example batch was found, so get one from the `.train` dataset
             result = next(iter(self.train))
